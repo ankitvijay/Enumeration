8a150b51bd20e21f589e7864dbc3550a44ba6f68
Ankit Vijay <ankitv@youi.com>
2020-05-18 06:45:57 +1000

    Initial code commit

---
+++ src/Enumeration/Enumeration.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AnkitVijay.Enumeration
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; }
+
+        public int Value { get; }
+
+        protected Enumeration(int value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+
+            if (otherValue == null)
+                return false;
+
+            var typeMatches = GetType() == obj.GetType();
+            var valueMatches = Value.Equals(otherValue.Value);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        {
+            var absoluteDifference = Math.Abs(firstValue.Value - secondValue.Value);
+            return absoluteDifference;
+        }
+
+        public static bool TryGetFromValueOrName<T>(
+            string valueOrName,
+            out T enumeration)
+            where T : Enumeration
+        {
+            return TryParse<T>(item => item.Name == valueOrName, out enumeration) ||
+                   int.TryParse(valueOrName, out var value) &&
+                   TryParse<T>(item => item.Value == value, out enumeration);
+        }
+
+        public static T FromValue<T>(int value) where T : Enumeration
+        {
+            var matchingItem = Parse<T, int>(value, "nameOrValue", item => item.Value == value);
+            return matchingItem;
+        }
+
+        public static T FromName<T>(string name) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(name, "name", item => item.Name == name);
+            return matchingItem;
+        }
+
+        private static bool TryParse<T>(Func<T, bool> predicate, out T enumeration)
+            where T : Enumeration
+        {
+            enumeration = GetAll<T>().FirstOrDefault(predicate);
+            return enumeration != null;
+        }
+
+        private static T Parse<T, TK>(TK nameOrValue, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+            {
+                throw new InvalidOperationException($"'{nameOrValue}' is not a valid {description} in {typeof(T)}");
+            }
+
+            return matchingItem;
+        }
+
+        public int CompareTo(object other) => Value.CompareTo(((Enumeration)other).Value);
+    }
+}
---
+++ src/Enumeration/Serialization/EnumerationNameIdConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace AnkitVijay.Enumeration.Serialization
+{
+    public class EnumerationNameConverter : JsonConverter
+    {
+        private const string NameProperty = "Name";
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is null)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                var name = value.GetType().GetProperty(NameProperty, BindingFlags.Public | BindingFlags.Instance);
+                if (name == null)
+                {
+                    throw new JsonSerializationException($"Error while writing JSON for {value}");
+                }
+
+                writer.WriteValue(name.GetValue(value));
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Integer:
+                case JsonToken.String:
+                    return GetFromNameOrValue(reader.Value.ToString(), objectType);
+                case JsonToken.StartObject:
+                case JsonToken.Null:
+                    return null;
+                default:
+                    throw new JsonSerializationException(
+                        $"Unexpected token {reader.TokenType} when parsing a smart enum.");
+            }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsSubclassOf(typeof(Enumeration));
+        }
+
+        private static object GetFromNameOrValue(string nameOrValue, Type objectType)
+        {
+            try
+            {
+                object result = default;
+                var methodInfo = typeof(Enumeration).GetMethod(
+                    nameof(Enumeration.TryGetFromValueOrName)
+                    , BindingFlags.Static | BindingFlags.Public);
+
+                if (methodInfo == null)
+                {
+                    throw new JsonSerializationException("Serialization is not supported");
+                }
+
+                var genericMethod = methodInfo.MakeGenericMethod(objectType);
+
+                var arguments = new[] { nameOrValue, result };
+
+                genericMethod.Invoke(null, arguments);
+                return arguments[1];
+            }
+            catch (Exception ex)
+            {
+                throw new JsonSerializationException($"Error converting value '{nameOrValue}' to a enumeration.", ex);
+            }
+        }
+    }
+}

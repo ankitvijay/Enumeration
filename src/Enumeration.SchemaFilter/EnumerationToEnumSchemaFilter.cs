082c382f6aa3f2ff821a0d476f6eedd34a9a54c3
Ankit Vijay <ankitv@youi.com>
2020-05-19 07:31:56 +1000

    Added Enumeration Schema Filter

---
+++ src/Enumeration.SchemaFilter/EnumerationToEnumSchemaFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Enumeration.SchemaFilter
+{
+    /// <inheritdoc />
+    public class EnumerationToEnumSchemaFilter : ISchemaFilter
+    {
+        /// <inheritdoc />
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (!context.Type.IsSubclassOf(typeof(AV.Enumeration.Enumeration)))
+            {
+                return;
+            }
+
+            var fields = context.Type.GetFields(BindingFlags.Static | BindingFlags.Public);
+
+            schema.Enum = fields.Select(field => new OpenApiString(field.Name)).Cast<IOpenApiAny>().ToList();
+            schema.Type = "string";
+            schema.Properties = null;
+            schema.AllOf = null;
+        }
+    }
+}

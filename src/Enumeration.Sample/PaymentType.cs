a50e8dc74e508f65b542b1aaa411f163f060e2a8
Ankit Vijay <ankitv@youi.com>
2020-05-19 07:42:56 +1000

    Added version 1

---
+++ src/Enumeration.Sample/PaymentType.cs
@@ -0,0 +1,13 @@
+﻿namespace AV.Enumeration.Sample.Version1
+{
+    public class PaymentType : Enumeration
+    {
+        public static readonly PaymentType DirectDebit = new PaymentType(1, nameof(DirectDebit));
+
+        public static readonly PaymentType CreditCard = new PaymentType(1, nameof(CreditCard));
+
+        private PaymentType(int value, string name) : base(value, name)
+        {
+        }
+    }
+}

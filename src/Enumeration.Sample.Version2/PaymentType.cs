b297ce6b609b70eba6d19434c9a27436776d2e31
Ankit Vijay <ankitv@youi.com>
2020-05-26 06:50:09 +1000

    Added Version 2 sample

---
+++ src/Enumeration.Sample.Version2/PaymentType.cs
@@ -0,0 +1,33 @@
+﻿namespace AV.Enumeration.Sample.Version2
+{
+    public abstract class PaymentType : Enumeration
+    {
+        public static readonly PaymentType DebitCard = new DebitCardType();
+
+        public static readonly PaymentType CreditCard = new CreditCardType();
+
+        public abstract string Code { get; }
+
+        private PaymentType(int value, string name = null) : base(value, name)
+        {
+        }
+
+        private class DebitCardType : PaymentType
+        {
+            public DebitCardType() : base(0, nameof(DebitCardType))
+            {
+            }
+
+            public override string Code => "DC";
+        }
+
+        private class CreditCardType : PaymentType
+        {
+            public CreditCardType() : base(1, nameof(DebitCardType))
+            {
+            }
+
+            public override string Code => "CC";
+        }
+    }
+}

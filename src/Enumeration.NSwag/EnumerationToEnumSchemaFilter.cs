4d6ca96612dd6b85c9b569ac6e174d285a4d7652
Ankit Vijay <ankitv@youi.com>
2020-07-04 15:00:33 +1000

    Added Enumeration.NSwag project

---
+++ src/Enumeration.NSwag/EnumerationToEnumSchemaFilter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AV.Enumeration.NSwag
+{
+    /// <summary>
+    /// Implements <see cref="ISchemaFilter"/> to transform <see cref="Enumeration"/> schema to <see cref="Enum"/>.
+    /// </summary>
+    internal class EnumerationToEnumSchemaFilter : ISchemaFilter
+    {
+        /// <inheritdoc />
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (!context.Type.IsSubclassOf(typeof(Enumeration)))
+            {
+                return;
+            }
+
+            var fields = context.Type.GetFields(BindingFlags.Static | BindingFlags.Public);
+
+            schema.Enum = fields.Select(field => new OpenApiString(field.Name)).Cast<IOpenApiAny>().ToList();
+            schema.Type = "string";
+            schema.Properties = null;
+            schema.AllOf = null;
+        }
+    }
+}
